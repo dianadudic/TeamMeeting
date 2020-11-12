d03a662bcfd78b9fb8941ce91b051f31948b2583
serjonchik <serjonchik@gmail.com>
2020-11-12 18:25:03 -0500

    changes in grater and less

--- src/Diana/task1.java
+++ src/Diana/task1.java
@@ -4,5 +4,7 @@ public class task1 {
     public static void main(String[] args) {
         //comment
         // just making sure am i able to write codes in your package also this is Serdar ..
+
+
     }
 }
--- src/SerdarAnnakurdov/SerdarsClass.java
+++ src/SerdarAnnakurdov/SerdarsClass.java
@@ -3,5 +3,13 @@ package SerdarAnnakurdov;
 public class SerdarsClass {
     public static void main(String[] args) {
         // this is my project
+        int n1 = 11;
+        int n2 = 23;
+
+        if(n1>n2){
+            System.out.println(n1+" is greater ");
+        }else{
+            System.out.println(n2+" is greater");
+        }
     }
 }
