199336b25eab3a799109a27e432535c988f807ae
serjonchik <serjonchik@gmail.com>
2020-11-12 17:57:55 -0500

    Serdar in Diannas Package

--- src/Diana/task1.java
+++ src/Diana/task1.java
@@ -3,5 +3,6 @@ package Diana;
 public class task1 {
     public static void main(String[] args) {
         //comment
+        // just making sure am i able to write codes in your package also this is Serdar ..
     }
 }
